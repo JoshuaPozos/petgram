32e1aa8bb9a01a2311d3128620eec2e8b033874b
Joshua Pozos <joshua@pozoss.com>
2020-01-29 16:19:19 -0600

    == React Hooks == * Added useEffect and useState hook Enhanced - ListOfCategories with useEffect and useState hook

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json";
 
-export const ListOfCategories = () => (
-  <List>
-    {categories.map(category => (
-      <Item key={category.id}>
-        <Category {...category} />
-      </Item>
-    ))}
-  </List>
-);
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function() {
+    fetch("https://petgram-server-plop.now.sh/categories")
+      .then(res => res.json())
+      .then(res => {
+        setCategories(res);
+      });
+  }, []);
+
+  return (
+    <List>
+      {categories.map(category => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
+    </List>
+  );
+};
